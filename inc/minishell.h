5ed6a283414da9cec0c8066f7c1f1f4387fcbf41
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-07-30 18:08:33 +0200

    Includes for allowed functions

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,5 +1,17 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <dirent.h>
+# include <signal.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
 
 #endif
