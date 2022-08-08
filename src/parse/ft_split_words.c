f5e418895be8dbc3c186cc843cf24690dac9ca2d
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-08-08 19:16:12 +0200

    8.8

---
+++ src/parse/ft_split_words.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_words.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 11:34:56 by amurcia-          #+#    #+#             */
+/*   Updated: 2022/08/08 19:14:42 by amurcia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static size_t	ft_len_word(char *str)
+{
+	size_t	cont;
+
+	cont = 0;
+	while (str[cont] && str[cont] != ' '
+		&& str[cont] != '\t' && str[cont] != '\n')
+		cont++;
+	return (cont);
+}
+
+static char	*ft_bidimensional(char *str)
+{
+	int		cont;
+	int		len;
+	char	*word;
+
+	cont = 0;
+	len = ft_len_word(str);
+	word = (char *)malloc(sizeof(char) * len + 1);
+	while (cont < len)
+	{
+		word[cont] = str[cont];
+		cont++;
+	}
+	word[cont] = '\0';
+	return (word);
+}
+
+static char	**ft_refile(char **array, char *str)
+{
+	size_t	cont;
+	size_t	cont_w;
+
+	cont = 0;
+	cont_w = 0;
+	while (str[cont] == ' ' || str[cont] == '\t' || str[cont] == '\n')
+		cont++;
+	while (str[cont] != '\0')
+	{
+		array[cont_w] = ft_bidimensional(str);
+		cont_w++;
+		while (str[cont] && str[cont] != ' '
+			&& str[cont] != '\t' && str[cont] != '\n')
+			cont++;
+		while (str[cont] == ' ' || str[cont] == '\t' || str[cont] == '\n')
+			cont++;
+	}
+	return (array);
+
+}
+
+static size_t	ft_count_words(char *str)
+{
+	size_t	words;
+	size_t	cont;
+
+	words = 0;
+	cont = 0;
+	while (str[cont] == ' ' || str[cont] == '\t' || str[cont] == '\n')
+		cont++;
+	while (str[cont])
+	{
+		while (str[cont] && str[cont] != ' '
+			&& str[cont] != '\t' && str[cont] != '\n')
+			cont++;
+		while (str[cont] == ' ' || str[cont] == '\t' || str[cont] == '\n')
+			cont++;
+		words++;
+	}
+	return (words);
+}
+
+char	**ft_split_words(char *str)
+{
+	char	**array;
+	size_t	words;
+
+	words = ft_count_words(str);
+	array = malloc(sizeof(char *) * (words + 1));
+	if (!array || !str)
+		return (NULL);
+	array[words] = NULL;
+	array = ft_refile(array, str);
+	return (array);
+}
