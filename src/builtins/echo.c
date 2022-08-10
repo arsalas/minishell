524f67c50d215acb7b8b5c92d3c6e2557f8b0dc7
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-08-10 12:37:00 +0200

    echo done, export no

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,55 +6,186 @@
 /*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 17:27:34 by aramirez          #+#    #+#             */
-/*   Updated: 2022/08/08 19:28:29 by amurcia-         ###   ########.fr       */
+/*   Updated: 2022/08/10 12:35:12 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_print_the_echo(char **words)
+void	ft_start_command(t_minishell *minishell, char *word)
 {
-	int		count;
+	minishell->start = 0;
+	while (word[minishell->start] && (word[minishell->start] == '\n' || word[minishell->start] == 't' || word[minishell->start]	== ' '))
+		minishell->start++;
+	if (minishell->flag == 0)
+		minishell->start += +4;
+	if (minishell->flag == 1)
+	{
+		minishell->start += +4;
+		while (word[minishell->start] && (word[minishell->start] == '\n' || word[minishell->start] == 't' || word[minishell->start]	== ' '))
+			minishell->start++;
+		minishell->start += +2;
+		while (word[minishell->start] == 'N' || word[minishell->start] == 'n')
+			minishell->start++;
+	}
+	while (word[minishell->start] && (word[minishell->start] == '\n' || word[minishell->start] == 't' || word[minishell->start]	== ' '))
+		minishell->start++;
+}
+
+//PRINTAMOS LO QUE HAY EN ECHO
+void	ft_print_the_echo(t_minishell *minishell, char *words)
+{
 	int		letter;
 
-	count = 1;
-	while (words[count])
+	ft_start_command(minishell, words);
+	letter = minishell->start;
+	while (words[letter])
 	{
-		letter = 0;
-		while (words[count][letter])
+		while (words[letter] && (words[letter] == '\n' || words[letter] == 't' || words[letter]	== ' '))
+			letter++;
+		if (words[letter] == 92 && (words[letter + 1] == 34 || words[letter + 1] == 39))
 		{
-			if (words[count][letter] != 34 && words[count][letter] != 39)
-			{
-				while (words[count][letter] && (words[count][letter] != 34 && words[count][letter] != 39))
-				{
-					printf("%c", words[count][letter]);
-					letter++;
-				}
-			}
-			else
-				letter++;
+			printf("%c", words[letter + 1]);
+			letter += 2;
 		}
-		if (words[count + 1] == NULL)
+		else if (words[letter] == 34)
+		{
+			letter++;
+			while (words[letter])
+			{
+				if (words[letter] == 92 && (words[letter + 1] == 34 || words[letter + 1] == 39))
+				{
+					printf("%c", words[letter + 1]);
+					letter += 2;
+				}
+				if (words[letter] == 34 && words[letter -1] != 92)
+				{
+					letter++;
+					break ;
+				}
+				printf("%c", words[letter]);
+				letter++;
+			}
+		}
+		else if (words[letter] == 39)
+		{
+			letter++;
+			while (words[letter])
+			{
+				if (words[letter] == 92 && (words[letter + 1] == 34 || words[letter + 1] == 39))
+				{
+					printf("%c", words[letter + 1]);
+					letter += 2;
+				}
+				if (words[letter] == 39 && words[letter -1] != 92)
+				{
+					letter++;
+					break ;
+				}
+				printf("%c", words[letter]);
+				letter++;
+			}
+		}
+		else
+		{
+			printf("%c", words[letter]);
+			letter++;
+		}
+		if (words[letter] == '\0' && minishell->flag != 1)
 		{
 			printf("\n");
 			return ;
 		}
-		printf(" ");
+	}
+}
+
+//MIRAMOS SI LAS COMILLAS ESTAN CERRADAS
+void	ft_quotes_error(t_minishell *minishell, char *inside)
+{
+	int	count;
+
+	count = 0;
+	while (inside[count])
+	{
+		if (inside[count] == 92 && (inside[count + 1] == 34 || inside[count + 1] == 39))
+			count += count + 2;
+		if (inside[count] == 34)
+		{
+			minishell->previous_double++;
+			count++;
+			while (inside[count])
+			{
+				if (inside[count] == 34 && inside[count -1] != 92)
+				{
+					minishell->previous_double++;
+					break ;
+				}
+				count++;
+			}
+		}
+		if (inside[count] == 39)
+		{
+			minishell->previous_simple++;
+			count++;
+			while (inside[count])
+			{
+				if (inside[count] == 39 && inside[count -1] != 92)
+				{
+					minishell->previous_simple++;
+					break ;
+				}
+				count++;
+			}
+		}
+		if (minishell->previous_simple % 2 != 0 || minishell->previous_double % 2 != 0)
+		{
+			printf("ERROR\nComillas no cerradas\n");
+			close_minishell();
+		}
 		count++;
 	}
 }
 
-void	ft_look_for_flag(char *inside_pipes)
+// MIRAMOS SI TENEMOS FLAG
+void	ft_look_for_flag(t_minishell *minishell, char *inside_pipes)
 {
 	char	**words;
+	int		cont;
 
+	cont = 2;
 	words = ft_split_words(inside_pipes);
-	if (words[1] != NULL &&
-		(ft_strcmp("-n", words[1]) || ft_strcmp("-N", words[2])))
+	if (words[1][0] == '-' && words[1][1] == 'n')
 	{
-		printf("ECHO -N\n");
-		return ;
+		while (words[1][cont] == 'n')
+			cont++;
+		if (words[1][cont] != '\0')
+		{
+			printf("ERROR\nEste flag no existe");
+			close_minishell();
+		}
+		minishell->flag = 1;
+		printf("TENEMOS FLAG\n");
 	}
-	ft_print_the_echo(words);
+	else if (words[1][0] == '-' && words[1][1] == 'N')
+	{
+		while (words[1][cont] == 'N')
+			cont++;
+		if (words[1][cont] != '\0')
+		{
+			printf("ERROR\nEste flag no existe\n");
+			close_minishell();
+		}
+		minishell->flag = 1;
+		printf("TENEMOS FLAG\n");
+	}
 	ft_free_split(words);
+}
+
+// TODAS LAS FUNCIONES PARA HACER EL ECHO
+void	ft_make_echo(t_minishell *minishell, char *inside_pipes)
+{
+	ft_look_for_flag(minishell, inside_pipes);
+	ft_odd_quotes(minishell, inside_pipes);
+	ft_quotes_error(minishell, inside_pipes);
+	ft_print_the_echo(minishell, inside_pipes);
 }
--- src/builtins/export.c
+++ src/builtins/export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 17:48:44 by aramirez          #+#    #+#             */
-/*   Updated: 2022/08/02 18:47:29 by aramirez         ###   ########.fr       */
+/*   Updated: 2022/08/10 12:02:20 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,65 @@
 void	export(char *name, char *content)
 {
 	push_env(name, content);
+}
+
+int	ft_end_export(int start, char *word)
+{
+	int	count;
+
+	count = 0;
+	while (word[start] != '\0' && word[start] != ' ' && word[start] != '\t' && word[start] != '\n')
+	{
+		start++;
+		count++;
+	}
+	return (count);
+}
+
+int	ft_start_export(char *word)
+{
+	int	count;
+
+	count = 0;
+	printf("WORD IS %s\n", word);
+	while (word[count] != '\0' && (word[count] == ' ' || word[count] == '\t' || word[count] == '\n'))
+		count++;
+	count += +6;
+	while (word[count] != '\0' && (word[count] == ' ' || word[count] == '\t' || word[count] == '\n'))
+		count++;
+	return (count);
+}
+
+void	ft_save_the_export(char *words, int start, int len)
+{
+	int		count;
+	char	*var;
+
+	count = 0;
+	var = (char *)malloc((sizeof(char) * len + 1));
+	if (!var)
+		return ;
+	printf("START ES %d\n", start);
+	printf("END ES %d\n", len);
+	var[len] = '\0';
+	printf("VAR LEN SHOULD BE NULL: %c\n", var[len]);
+	while (words[start])
+	{
+		var[count] = words[start];
+		start++;
+		count++;
+	}
+	printf("VAR IS %s\n", var);
+}
+
+void	ft_make_export(t_minishell *minishell, char *inside_pipes)
+{
+	int	start;
+	int	len;
+
+	ft_odd_quotes(minishell, inside_pipes);
+	ft_quotes_error(minishell, inside_pipes);
+	start = ft_start_export(inside_pipes);
+	len = ft_end_export(start, inside_pipes);
+	ft_save_the_export(inside_pipes, start, len);
 }
