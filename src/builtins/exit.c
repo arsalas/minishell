1b45c2207f2571ad5f6613165432c6e1180cd2b1
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-08-11 18:11:48 +0200

    Add files via upload

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,41 +6,73 @@
 /*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 17:31:29 by aramirez          #+#    #+#             */
-/*   Updated: 2022/08/11 11:01:11 by amurcia-         ###   ########.fr       */
+/*   Updated: 2022/08/11 18:02:46 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
+* Nos saltamos los espacios, \t o \n que haya en el string
+*/
+int	ft_empty_piece(char *str, int count)
+{
+	while (str[count] && (str[count] == '\n'
+			|| str[count] == '\t' || str[count] == ' '))
+		count++;
+	return (count);
+}
+
+/*
+* Nos posicionamos despues de la palabra exit y los espacios
+*/
+int	ft_after_exit(char *str, int count)
+{
+	count = ft_empty_piece(str, count);
+	count += 4;
+	count = ft_empty_piece(str, count);
+	return (count);
+}
+
+/*
+* Damos error en caso de que nos den un no digit despues de exit
+*/
+int	ft_its_not_digit(char *str, int len)
+{
+	int		count;
+	char	*str_error;
+
+
+	count = ft_strlen(str);
+	str_error = (char *)malloc((sizeof(char) * count + 1));
+	count = 0;
+	while (str[len] && !(ft_isdigit(str[len])))
+	{
+		str_error[count] = str[len];
+		len++;
+		count++;
+	}
+	printf("exit: %s: numeric argument required\n", str_error);
+	return (-1);
+}
+
+/*
+* Realizamos la funcion exit
+*/
 void	ft_exit(char *str)
 {
 	int		count;
-	int		len;
-	char	*str_error;
 
-	count = ft_strlen(str);
-	len = 0;
-	str_error = (char *)malloc((sizeof(char) * count + 1));
 	count = 0;
 	while (str[count])
 	{
-		while (str[count] && (str[count] == '\n' || str[count] == '\t' || str[count] == ' '))
-			count++;
-		count += 4;
-		while (str[count] && (str[count] == '\n' || str[count] == '\t' || str[count] == ' '))
-			count++;
+		count = ft_after_exit(str, count);
 		while (str[count])
 		{
 			if (!(ft_isdigit(str[count])))
 			{
-				while (str[count] && !(ft_isdigit(str[count])))
-				{
-					str_error[len] = str[count];
-					len++;
-					count++;
-				}
-				printf("exit: %s: numeric argument required\n", str_error);
-				return ;
+				if (ft_its_not_digit(str, count) == -1)
+					return ;
 			}
 			count++;
 		}
