d72fa925c66673cd1f0946b33084865496eed794
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-08-17 19:19:30 +0200

    memory progress

--- inc/input.h
+++ inc/input.h
@@ -6,17 +6,16 @@
 /*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 13:57:35 by aramirez          #+#    #+#             */
-/*   Updated: 2022/08/13 18:43:30 by aramirez         ###   ########.fr       */
+/*   Updated: 2022/08/17 16:36:27 by aramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef INPUT_H
 # define INPUT_H
 
-void    req_new_input(void);
-int     get_quantity_process_in_input(char *input);
-char    *extract_content_process_input(char *input);
-char    *extract_others_process_input(char *input);
+void	req_new_input(void);
+int		get_quantity_process_in_input(char *input);
+char	*extract_content_process_input(char *input);
+char	*extract_others_process_input(char *input);
 
 #endif
