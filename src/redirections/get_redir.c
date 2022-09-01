48cbab514b23334ddeb5d680e520fff1b4a69da2
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-09-01 17:49:29 +0200

    redirs

--- src/redirections/get_redir.c
+++ src/redirections/get_redir.c
@@ -6,7 +6,7 @@
 /*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 17:33:23 by amurcia-          #+#    #+#             */
-/*   Updated: 2022/08/30 19:19:59 by aramirez         ###   ########.fr       */
+/*   Updated: 2022/09/01 17:48:09 by aramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,12 +59,14 @@ t_fd_redirs	ft_get_redir(t_pipe command)
 	{
 		if (command.redirs.info[i].types == REIN)
 			fds.input = open(command.redirs.info[i].files, O_RDONLY);
-		else if (command.redirs.info[i].types == DOUBBLE_REIN)
-			fds.input = open(command.redirs.info[i].files, O_RDONLY);
 		else if (command.redirs.info[i].types == REOUT)
 			fds.output = open(command.redirs.info[i].files, O_CREAT | O_RDWR | O_TRUNC, 0666);
-		else
+		else if (command.redirs.info[i].types == DOUBBLE_REOUT)
 			fds.output = open(command.redirs.info[i].files, O_CREAT | O_RDWR | O_APPEND, 0666);
+		else
+		{
+			fds.input = open(command.redirs.info[i].files, O_RDONLY);
+		}		
 		ft_redir_errors(command.redirs.info[i].types, command.redirs.info[i].files);
 		i++;
 	}
