9eec76c1f1aca449d1f5031f591de1bf19ac17a6
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2022-11-09 20:59:31 +0100

    nada funciona

--- src/utils/string_4.c
+++ src/utils/string_4.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   string_4.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aramirez <aramirez@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amurcia- <amurcia-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 19:23:15 by aramirez          #+#    #+#             */
-/*   Updated: 2022/11/09 19:32:35 by aramirez         ###   ########.fr       */
+/*   Updated: 2022/11/09 20:58:01 by amurcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ char	*parse_token2(char *input, int i)
 {
 	char	*aux;
 
-	*i = *i + 1;
+	i++;
 	aux = parse_simple_quote(&input[i]);
 	while (input[i] != '\'')
-		*i = *i + 1;
-	*i = *i + 1;
+		i++;
+	i++;
 	return (aux);
 }
 
@@ -28,15 +28,15 @@ char	*parse_token3(char *input, int i)
 {
 	char	*aux;
 
-	*i = *i + 1;
+	i++;
 	if (!input[i])
 		aux = ft_strdup("$");
 	else
 		aux = parse_expand_var(&input[i]);
 	if (!(ft_isalpha(input[i]) == 1 || input[i] == '_'))
-		*i = *i + 1;
+		i++;
 	while (input[i] && (ft_isalnum(input[i]) == 1 || input[i] == '_'))
-		*i = *i + 1;
+		i++;
 	return (aux);
 }
 
@@ -44,12 +44,12 @@ char	*parse_token4(char *input, int i)
 {
 	char	*aux;
 
-	*i = *i + 1;
+	i++;
 	aux = parse_double_quote(&input[i]);
 	aux = parse_double_quotes(aux);
 	while (input[i] != '"')
-		*i = *i + 1;
-	*i = *i + 1;
+		i++;
+	i++;
 	return (aux);
 }
 
@@ -61,7 +61,7 @@ char	*parse_token5(char *input, int i)
 	start = i;
 	while (input[i]
 		&& (input[i] != '"' && input[i] != '$' && input[i] != '\''))
-		*i = *i + 1;
+		i++;
 	aux = ft_substr_mod(input, start, i - start);
 	return (aux);
 }
